447eeb2636ba0d351cb8f1dd83c4d9b65d9babd2
laskor <al.moksud@gmail.com>
2022-03-08 01:22:32 -0500

    url added

--- js/app.js
+++ js/app.js
@@ -7,5 +7,23 @@ const searchFood = () => {
     // console.log(url);
      fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals))
+        .then(data => displaySearchResult(data.meals))
+}
+
+const displaySearchResult = meals => {
+    const searchR = document.getElementById('search-result');
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card shadow-lg">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title fw-bold text-danger">${meal.strMeal}</h5>
+                <p class="card-text text-md-start "><span class="fw-bold text-success">Cooking Instructions : </span>${meal.strInstructions}</p>
+            </div>
+        </div>
+        `;
+        searchR.appendChild(div);
+    });
 }
